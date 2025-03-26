c6155d2b3daa3bb9a7e8560e38cebc6bd8d466fa
Kika <49309849+kika0522@users.noreply.github.com>
2025-03-27 02:24:20 +0500

    test

--- app/main.py
+++ app/main.py
@@ -109,7 +109,7 @@ async def get_report(
                 "message": "Отчет в процессе генерации"
             }
         return {
-            "task_id": report.id,
+            "task_id": report.status,
             "status": report.status,
             "results": results_data,
         }
